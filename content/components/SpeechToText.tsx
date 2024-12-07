16fc8d6f3464c368194fcc3043e6bb4dbf653304
pintoinfant <pintoinfant5650@gmail.com>
2024-12-08 04:56:49 +0530

    Embeddings

--- content/components/SpeechToText.tsx
+++ content/components/SpeechToText.tsx
@@ -121,136 +121,157 @@
 
 // export default SpeechToText;
 import React, { useState, useEffect } from "react";
-import {getEmbedding} from "../utils/getEmbeddings"
+
 
 const SpeechToText = () => {
-  const [text, setText] = useState(""); // To store the recognized text
-  const [isListening, setIsListening] = useState(false);
-  const [error, setError] = useState("");
+    const [text, setText] = useState(""); // To store the recognized text
+    const [isListening, setIsListening] = useState(false);
+    const [error, setError] = useState("");
 
-  let recognition: any;
+    let recognition: any;
 
-  // Check if SpeechRecognition is available
-  if ("webkitSpeechRecognition" in window || "SpeechRecognition" in window) {
-    const SpeechRecognition =
-      window.SpeechRecognition || window.webkitSpeechRecognition;
-    recognition = new SpeechRecognition();
+    // Check if SpeechRecognition is available
+    if ("webkitSpeechRecognition" in window || "SpeechRecognition" in window) {
+        const SpeechRecognition =
+            window.SpeechRecognition || window.webkitSpeechRecognition;
+        recognition = new SpeechRecognition();
 
-    // SpeechRecognition configuration
-    recognition.continuous = true; // Keep listening for speech
-    recognition.interimResults = false; // Only return final results
-    recognition.lang = "en-US"; // Language for recognition
-  } else {
-    console.error("SpeechRecognition not supported in this browser");
-    setError("SpeechRecognition not supported in this browser");
-  }
-
-  // Start listening to speech
-  const startListening = () => {
-    if (!recognition) {
-      return;
+        // SpeechRecognition configuration
+        recognition.continuous = true; // Keep listening for speech
+        recognition.interimResults = false; // Only return final results
+        recognition.lang = "en-US"; // Language for recognition
+    } else {
+        console.error("SpeechRecognition not supported in this browser");
+        setError("SpeechRecognition not supported in this browser");
     }
 
-    recognition.start();
-    setIsListening(true);
-
-    // Event listeners
-    recognition.onresult = async (event) => {
-      let spokenText = "";
-      for (let i = event.resultIndex; i < event.results.length; i++) {
-        // Check if the result is final (i.e., the speech has ended)
-        if (event.results[i].isFinal) {
-            spokenText += event.results[i][0].transcript;
+    // Start listening to speech
+    const startListening = () => {
+        if (!recognition) {
+            return;
         }
-      }
-      if(spokenText != "") {
-          const embed = await getEmbedding(spokenText)
-          console.log(embed.data[0].embedding)
-      }
+
+        recognition.start();
+        setIsListening(true);
+
+        // Event listeners
+        recognition.onresult = async (event) => {
+            let spokenText = "";
+            for (let i = event.resultIndex; i < event.results.length; i++) {
+                // Check if the result is final (i.e., the speech has ended)
+                if (event.results[i].isFinal) {
+                    spokenText += event.results[i][0].transcript;
+                }
+            }
+            if (spokenText != "") {
+                const insertIntoDB = async () => {
+                    const meetingId = window.location.href.split("/").pop();
+
+                    await fetch("http://localhost:3000/insert-embeddings", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            meetingId,
+                            query: spokenText,
+                        })
+                    }).then((response) => {
+                        if (response.ok) {
+                            console.log("Successfully inserted into DB");
+                        } else {
+                            console.error("Failed to insert into DB");
+                        }
+                    }).catch((error) => {
+                        console.error("Error inserting into DB: ", error);
+                    })
+                }
+                insertIntoDB();
+            }
 
 
-      // Only update the text state when we have the final result
-      if (spokenText) {
-        setText((prevText) => prevText + " " + spokenText);
-      }
+            // Only update the text state when we have the final result
+            if (spokenText) {
+                setText((prevText) => prevText + " " + spokenText);
+            }
+        };
+
+        recognition.onerror = (event) => {
+            console.error("Error occurred in speech recognition: ", event.error);
+            setError(event.error);
+            setIsListening(false);
+        };
+
+        recognition.onend = () => {
+            setIsListening(false);
+        };
     };
 
-    recognition.onerror = (event) => {
-      console.error("Error occurred in speech recognition: ", event.error);
-      setError(event.error);
-      setIsListening(false);
-    };
-
-    recognition.onend = () => {
-      setIsListening(false);
-    };
-  };
-
-  // Stop listening to speech
-  const stopListening = () => {
-    if (!recognition) {
-      return;
-    }
-    recognition.stop();
-    setIsListening(false);
-  };
-
-  useEffect(() => {
-    // Clean up event listeners on component unmount
-    return () => {
-      if (recognition) {
+    // Stop listening to speech
+    const stopListening = () => {
+        if (!recognition) {
+            return;
+        }
         recognition.stop();
-        recognition.onresult = null;
-        recognition.onerror = null;
-        recognition.onend = null;
-      }
+        setIsListening(false);
     };
-  }, []);
 
-  return (
-    <div>
-      <p className="mt-6 text-lg leading-8 text-gray-300">
-        Speech-to-Text Recognition
-      </p>
-      <div>
-        <button
-          onClick={startListening}
-          disabled={isListening}
-          className="bg-zinc-100 py-1 px-2 rounded-xl text-black-500 text-md hover:bg-zinc-200 hover:shadow-xl focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white mt-2 font-bold my-4 mr-2"
-        >
-          Start Listening
-        </button>
-        <button
-          onClick={stopListening}
-          disabled={!isListening}
-          className="bg-zinc-100 py-1 px-2 rounded-xl text-black-500 text-md hover:bg-zinc-200 hover:shadow-xl focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white mt-2 font-bold my-4 mr-2"
-        >
-          Stop Listening
-        </button>
-      </div>
+    useEffect(() => {
+        // Clean up event listeners on component unmount
+        return () => {
+            if (recognition) {
+                recognition.stop();
+                recognition.onresult = null;
+                recognition.onerror = null;
+                recognition.onend = null;
+            }
+        };
+    }, []);
 
-      {isListening && (
-        <p className="mt-1 text-green-500">
-          Starting to transcribe your call...
-        </p>
-      )}
+    return (
+        <div>
+            <p className="mt-6 text-lg leading-8 text-gray-300">
+                Speech-to-Text Recognition
+            </p>
+            <div>
+                <button
+                    onClick={startListening}
+                    disabled={isListening}
+                    className="bg-zinc-100 py-1 px-2 rounded-xl text-black-500 text-md hover:bg-zinc-200 hover:shadow-xl focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white mt-2 font-bold my-4 mr-2"
+                >
+                    Start Listening
+                </button>
+                <button
+                    onClick={stopListening}
+                    disabled={!isListening}
+                    className="bg-zinc-100 py-1 px-2 rounded-xl text-black-500 text-md hover:bg-zinc-200 hover:shadow-xl focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white mt-2 font-bold my-4 mr-2"
+                >
+                    Stop Listening
+                </button>
+            </div>
 
-      {!isListening && text && (
-        <p className="mt-1 text-red-500">Transcribing stopped.</p>
-      )}
+            {isListening && (
+                <p className="mt-1 text-green-500">
+                    Starting to transcribe your call...
+                </p>
+            )}
 
-      {error && <p style={{ color: "red" }}>Error: {error}</p>}
+            {!isListening && text && (
+                <p className="mt-1 text-red-500">Transcribing stopped.</p>
+            )}
 
-      <textarea
-        rows={10}
-        cols={50}
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Speech will be converted to text here..."
-        className="mt-4 p-4 w-full bg-gray-100 rounded-lg shadow-md text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
-    </div>
-  );
+            {error && <p style={{ color: "red" }}>Error: {error}</p>}
+
+            <textarea
+                rows={10}
+                cols={50}
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="Speech will be converted to text here..."
+                className="mt-4 p-4 w-full bg-gray-100 rounded-lg shadow-md text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+        </div>
+    );
 };
 
 export default SpeechToText;
