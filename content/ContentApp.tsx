e7f0e3b1dd2e8798325d715fc9ac03378b8ee7ed
Subhiksha18vic <esubivictoria129@gmail.com>
2024-12-07 14:40:24 +0530

    Added alerts

--- content/ContentApp.tsx
+++ content/ContentApp.tsx
@@ -1,3 +1,140 @@
+// import React, { useState, useEffect } from "react";
+// import logo from "../public/logo.webp";
+// import chromeWindowBg from "../public/chromeWindow.png";
+// import Layout from "./components/Layout";
+// import { Providers } from "./Providers";
+// import Chat from "./components/Chat";
+// import processAccount from "./utils/processAccount";
+// import processTx from "./utils/processTx";
+// import SpeechText from "./components/SpeechToText.tsx";
+
+// // async function parseUrlToJson(
+// //   url: string
+// // ): Promise<{ type: string; value: any }> {
+// //   try {
+// //     const regex =
+// //       /https:\/\/beryx.io\/search\/fil\/mainnet\/(address|txs)\/([^?]+)/;
+// //     const match = url.match(regex);
+
+// //     if (match && match.length >= 3) {
+// //       const type = match[1];
+// //       const value = match[2];
+
+// //       if (type === "txs") {
+// //         const data = await processTx(value);
+// //         return {
+// //           type: "tx",
+// //           value: data,
+// //         };
+// //       } else {
+// //         const data = await processAccount(value);
+// //         return {
+// //           type: "address",
+// //           value: data,
+// //         };
+// //       }
+// //     }
+// //   } catch (error) {
+// //     console.error(error);
+// //     return {
+// //       type: "error",
+// //       value: error,
+// //     };
+// //   }
+
+// //   return {
+// //     type: "error",
+// //     value: "Invalid URL",
+// //   };
+// // }
+
+// export default function ContentApp() {
+//   const [isdialogOpen, setIsDialogOpen] = useState(true);
+//   const [chatActive, setChatActive] = useState(false);
+//   const [currentData, setCurrentData] = useState<any>({});
+//   const [roomId, setRoomId] = useState("");
+
+//   async function initSmith() {
+//     // Get URL of the current tab
+//     const url = window.location.href;
+//     const currentRoom = url.split("/").pop();
+//     alert(
+//       `Hudson is ready to help you with the meeting. Please click on the chat button to start the conversation. ${currentRoom}`
+//     );
+
+//     setRoomId(currentRoom as string);
+//     setChatActive(true);
+//   }
+
+//   if (!isdialogOpen) {
+//     return (
+//       <div className="mx-auto p-6">
+//         <button
+//           onClick={() => setIsDialogOpen(true)}
+//           className="bg-white rounded-md p-3 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+//         >
+//           Call Smith <span aria-hidden="true">+</span>
+//         </button>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <Providers>
+//       <div className="mx-auto max-w-7xl md:px-0 lg:p-6">
+//         <Layout>
+//           <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl lg:rounded-3xl md:pt-24 md:h-full sm:h-[100vh] lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+//             <div className="mx-auto max-w-md text-center lg:py-12 lg:mx-0 lg:flex-auto lg:text-left">
+//               <div className="flex items-center justify-start space-x-4 my-4 mx-auto">
+//                 <img
+//                   alt="logo"
+//                   src={logo}
+//                   className="relative inline-block w-20 rounded-xl"
+//                 />
+//               </div>
+//               <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+//                 Chat with Smith, your personalized AI assistant
+//               </h2>
+//               <p className="mt-6 text-lg leading-8 text-gray-300">
+//                 Smith is a conversational AI assistant that helps you with your
+//                 meetings, and notes for your meetings. It also acts as a second
+//                 brain to help you take notes faster.
+//               </p>
+//               {!chatActive ? (
+//                 <button
+//                   onClick={initSmith}
+//                   className="bg-zinc-100 py-1 px-2 rounded-lg text-zinc-800 text-md hover:bg-zinc-200 hover:shadow-xl focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white mt-2 font-bold"
+//                 >
+//                   Call Smith to initiate chat
+//                 </button>
+//               ) : (
+//                 // <Chat data={currentData} />
+//                 <SpeechText />
+//               )}
+//               <div>
+//                 <button
+//                   onClick={() => setIsDialogOpen(false)}
+//                   className="mt-4 underline hover:no-underline"
+//                 >
+//                   Click to send smith away.
+//                 </button>
+//               </div>
+//             </div>
+//             <div className="relative mt-16 h-80 lg:mt-8">
+//               <img
+//                 className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+//                 src={chromeWindowBg}
+//                 alt="Chrome window screenshot"
+//                 width="1824"
+//                 height="1080"
+//               />
+//             </div>
+//           </div>
+//         </Layout>
+//       </div>
+//     </Providers>
+//   );
+// }
 import React, { useState, useEffect } from "react";
 import logo from "../public/logo.webp";
 import chromeWindowBg from "../public/chromeWindow.png";
@@ -6,7 +143,7 @@ import { Providers } from "./Providers";
 import Chat from "./components/Chat";
 import processAccount from "./utils/processAccount";
 import processTx from "./utils/processTx";
-import SpeechText from "./components/SpeechToText.tsx"
+import SpeechText from "./components/SpeechToText.tsx";
 
 // async function parseUrlToJson(
 //   url: string
@@ -59,7 +196,7 @@ export default function ContentApp() {
     const url = window.location.href;
     const currentRoom = url.split("/").pop();
     alert(
-      `Smith is ready to help you with the meeting. Please click on the chat button to start the conversation. ${currentRoom}`
+      `Hudson is ready to help you with the meeting. Please click on the chat button to start the conversation. ${currentRoom}`
     );
 
     setRoomId(currentRoom as string);
@@ -93,11 +230,14 @@ export default function ContentApp() {
                 />
               </div>
               <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                Chat with Smith, your personalized AI assistant
+                Chat with Hudson, your personalized Meet AI assistant
               </h2>
-              <p className="mt-6 text-lg leading-8 text-gray-300">
-                Smith is a conversational AI assistant that helps you with your meetings, and notes for your meetings.
-                It also acts as a second brain to help you take notes faster.
+              <p className="mt-6 text-md leading-8 text-gray-300">
+                Hudson an AI-powered assistant that captures and stores meeting
+                discussions in real time. With simple voice queries, you can
+                retrieve any information or insights from past meetings
+                instantly. Simply ask, and get accurate, context-aware responses
+                from your stored meeting data.
               </p>
               {!chatActive ? (
                 <button
@@ -128,6 +268,15 @@ export default function ContentApp() {
                 height="1080"
               />
             </div>
+
+            {/* Close Button (Top-right) */}
+            <button
+              onClick={() => setIsDialogOpen(false)}
+              className="absolute top-4 right-4 text-white text-xl font-bold hover:text-gray-400"
+              aria-label="Close"
+            >
+              &times;
+            </button>
           </div>
         </Layout>
       </div>
